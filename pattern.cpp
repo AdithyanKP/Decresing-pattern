2a9939ae8a6da01289a5368e21cad759b6a7a36e
Adithyan k P <adithyankp777@gmail.com>
2023-03-26 14:50:45 +0530

    reverse pattern

---
+++ pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    for (int i = a; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
