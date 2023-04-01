f173359117bd3e539b69f085f3bbd637cd9face0
Adithyan k P <adithyankp777@gmail.com>
2023-04-01 14:07:54 +0530

    right side pattern added

--- rightsided.cpp
+++ rightsided.cpp
@@ -7,10 +7,15 @@ int main()
     int a = 5;
     for (int i = 1; i <= a; i++)
     {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "  ";
+        }
         for (int j = i; j <= a; j++)
         {
             cout << "* ";
         }
+
         cout << "\n";
     }
     return 0;
